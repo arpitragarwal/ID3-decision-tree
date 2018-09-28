fe8585c59d1482e6d508549dcdae5ee90fe92cca
Arpit Agarwal <arpit.r.agarwal@gmail.com>
2018-09-27 20:42:26 -0500

    mend

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear; clc; close all; format compact;
 
-filename = 'data/heart_train.arff.txt';
+filename = 'data/play_tennis.arff.txt';
 [data, metadata] = read_arff_file(filename);
 fclose all;
 
@@ -10,7 +10,7 @@ entropy = compute_entropy(data, metadata);
 
 %children_data_sets = split_data_sets(data, metadata, splits, attribute_number);
 
-for i = 1:13
+for i = 1:length(metadata.attribute_names)
     attribute_number = i;
     gain(i) = info_gain(data, metadata, splits, attribute_number);
 end
