99119ad984deb211777013f322555fd693214fc5
Arpit Agarwal <arpit.r.agarwal@gmail.com>
2018-09-30 23:51:22 -0500

    Cleanup
    Signed-off-by: Arpit Agarwal <arpit.r.agarwal@gmail.com>

---
+++ make_predictions.m
@@ -0,0 +1,23 @@
+function [number_correctly_classified, total_no_of_test_instances] = make_predictions(test_data, tree)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% evaluate accuracy
+
+for i = 1:length(test_data(:, 1))
+    sample{i, :} = test_data(i, :);
+    predicted_label{i, :} = tree.find_correct_child(sample{i, :});
+end
+
+for i = 1:length(test_data(:, 1))
+    is_predict_correct(i, 1) = strcmp(predicted_label{i}, test_data{i, end});
+end
+
+number_correctly_classified = sum(is_predict_correct);
+total_no_of_test_instances = length(test_data(:, 1));
+
+for i = 1:length(test_data(:, 1))
+    disp([num2str(i), ': Actual: ', test_data{i, end}, ' Predicted: ', predicted_label{i}]);
+end
+disp(['Number of correctly classified: ', num2str(number_correctly_classified), ' Total number of test instances: ', num2str(total_no_of_test_instances)]);
+end
+
